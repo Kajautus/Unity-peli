45983c4f620eae41a381ce1eb1dde6fc1653ef3b
Tondede <110910561+Tondede@users.noreply.github.com>
2022-10-04 15:21:30 +0300

    Made enemy ai that shoots player

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -7,14 +7,119 @@ public class EnemyFollow : MonoBehaviour
 {
     public NavMeshAgent enemy;
     public Transform Player;
-    void Start()
+    public LayerMask whatIsPlayer, whatIsGround;
+
+    public float health;
+    public ParticleSystem particles;
+    public AudioClip destroySound;
+    public AudioSource damageSound;
+
+    //Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
     {
+        Player = GameObject.Find("FPS_Player").transform;
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            enemy.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = Player.transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer ()
+    {
+        enemy.SetDestination(Player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        enemy.SetDestination(transform.position);
+
+        transform.LookAt(Player);
         
+        if (!alreadyAttacked)
+        {
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            
+            rb.AddForce(transform.forward * 50f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 4f, ForceMode.Impulse);
+
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage (int damage)
+    {
+        damageSound.Play();
+        health -= damage;
+
+        if (health <= 0) Invoke(nameof(DestroyEnemy), 5f);
+    }
+    private void DestroyEnemy()
+    {
+        AudioSource.PlayClipAtPoint(destroySound, transform.position, 2f);
+        Instantiate(particles, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemy.SetDestination(Player.position);
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
